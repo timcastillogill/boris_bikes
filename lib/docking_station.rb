21499f6756a14e0952b273491d2882bb8a5ef055
Tim Castillo-Gill <timgillviola@gmail.com>
2020-09-30 16:18:33 +0100

    Wrote one line test in spec file

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,8 @@
-DockingStation = Class.new
 
 class DockingStation
 
+  def release_bike
+    
+  end
+  
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,17 @@
 require 'docking_station.rb'
 
+# describe DockingStation do
+#   it "will release a bike when called" do
+#     expect(subject).to respond_to :release_bike
+#   end
+# end
+
+# describe DockingStation do
+#   it "release a bike from the station" do
+#     expect(DockingStation.new).to respond_to release_bike
+#   end
+# end
+ 
 describe DockingStation do
-  it "will release a bike when called" do
-    expect(subject).to respond_to :release_bike
-  end
+  it { DockingStation.new is_expected.to respond_to :release_bike }
 end
